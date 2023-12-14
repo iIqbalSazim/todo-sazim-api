19e47bfc196bff3646bc7ee1d1504c330ae355ec
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-12-14 15:45:53 +0600

    Add test suite for tasks model.
    - Add task_test file and write unit tests.
    - Install shoulda to write validation checks.

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem "mocha"
 gem "byebug"
 
 gem "json"
+
+gem 'shoulda'
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,25 @@
-require "test_helper"
+require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:description)
+  should validate_presence_of(:priority)
+  should validate_presence_of(:due_date)
+
+  test 'task is created with validations' do
+    valid_task = Task.new(
+      description: 'Valid Description',
+      priority: 'high',
+      due_date: Date.today
+    )
+    assert valid_task.valid?
+    assert_empty valid_task.errors
+
+    invalid_task = Task.new(
+      description: '',
+      priority: 'high',
+      due_date: Date.today
+    )
+    assert_not invalid_task.valid?
+    assert_not_empty invalid_task.errors[:description]
+  end
 end
